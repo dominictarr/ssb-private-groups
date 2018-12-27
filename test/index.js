f8b540f929cf4de61e694622166b3a1e1505f22a
Dominic Tarr <dominic.tarr@gmail.com>
2018-12-28 09:28:42 +1300

    tidy tests

--- test/index.js
+++ test/index.js
@@ -2,14 +2,20 @@ var chloride = require('chloride')
 var tape = require('tape')
 var group_box = require('group-box')
 var u = require('../util')
+var ssbKeys = require('ssb-keys')
 var Scuttlebot = require('ssb-server')
   .use(require('../'))
 
+function hash (s) {
+  return chloride.crypto_hash_sha256(Buffer.from(s, 'utf8'))
+}
+
 var alice = Scuttlebot({
-  temp: true
+  temp: true,
+  keys: ssbKeys.generate(null, hash('alice_secret1'))
 })
 
-var bob = alice.createFeed()
+var bob = alice.createFeed(ssbKeys.generate(null, hash('bob_secret1')))
 
 function generate (seed) {
   var keys = chloride.crypto_box_seed_keypair(seed)
@@ -26,11 +32,8 @@ function toBuffer(s) {
 function scalarmult (pk,sk) {
   return chloride.crypto_scalarmult(toBuffer(pk), toBuffer(sk))
 }
-function hash (s) {
-  return chloride.crypto_hash_sha256(Buffer.from(s, 'utf8'))
-}
-var alice_keys = generate(hash('alice_secret'))
-var bob_keys = generate(hash('bob_secret'))
+var alice_keys = generate(hash('alice_secret2'))
+var bob_keys = generate(hash('bob_secret2'))
 
 tape('create a private-msg-key', function (t) {
   alice.privateGroups.addCurvePair(alice_keys, function (err) {
@@ -49,7 +52,6 @@ tape('create a private-msg-key', function (t) {
         key: bob_keys.public
       }, function (err, data) {
         if(err) throw err
-        console.log(data)
         t.ok(data.key)
 
         var content = { type: 'private', text: 'hello, alice' }
@@ -58,48 +60,19 @@ tape('create a private-msg-key', function (t) {
         var keys = [bob_keys, alice_keys].map(function (key) {
           return scalarmult(bob_keys.private, key.public)
         })
-        var keys2 = [bob_keys].map(function (key) {
-          return scalarmult(alice_keys.private, key.public)
-        })
-        console.log('plaintext.length', ptxt.length)
-
-        var ctxt = group_box.box(
-          ptxt,
-          nonce,
-          keys
-        )
-//        console.log("CTXT", ctxt.toString('base64'))
-//        console.log("NONCE", nonce)
-//        console.log("KEYS", keys)
-//        console.log("KEYS2", keys2)
         var _key = group_box.unboxKey(ctxt, nonce, keys, 8)
-        console.log("INPUT", {
-          ctxt: ctxt.toString('hex'),
-          nonce: nonce,
-          key: _key
-        })
-        console.log(
-          'INPUT_TEST',
-          chloride.crypto_hash_sha256(Buffer.concat([ctxt, nonce, _key])),
-group_box.unboxBody(ctxt, nonce, _key).toString()
-        )
-        console.log('d.ptxt', group_box.unboxBody(ctxt, nonce, _key).toString())
-
-//        console.log('ctxt.length', ctxt.length)
+        t.ok(_key, 'message can be decrypted')
         bob.publish(
           ctxt.toString('base64')+'.box2',
           function (err, data) {
             if(err) throw err
             t.ok(data)
-//            console.log(data)
-//            alice.privateGroups.get(function () {
-              alice.get({id: data.key, private: true}, function (err, msg) {
-                if(err) throw err
-                t.deepEqual(msg.content, content)
-                t.end()
+            alice.get({id: data.key, private: true}, function (err, msg) {
+              if(err) throw err
+              t.deepEqual(msg.content, content)
+              t.end()
 
-              })
-  //          })
+            })
           }
         )
       })
@@ -111,7 +84,4 @@ tape('cleanup', function (t) {
   alice.close()
   t.end()
 })
-
-
-
 
